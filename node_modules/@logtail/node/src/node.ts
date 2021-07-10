3c4ceb032ffb27003f1392ed4aa87d879c182f9c
Simon Rozsival <simon@rozsival.com>
2021-07-10 11:07:17 +0200

    Modified default nextjs boilerplate app

---
+++ node_modules/@logtail/node/src/node.ts
@@ -0,0 +1,92 @@
+import { Duplex, Writable } from "stream";
+
+import fetch from "cross-fetch";
+import { encode } from "@msgpack/msgpack";
+
+import { ILogtailLog, Context, ILogtailOptions, LogLevel } from "@logtail/types";
+import { Base } from "@logtail/core";
+
+import { getStackContext } from "./context";
+
+export class Node extends Base {
+  /**
+   * Readable/Duplex stream where JSON stringified logs of type `ILogtailLog`
+   * will be pushed after syncing
+   */
+  private _writeStream?: Writable | Duplex;
+
+  public constructor(
+    sourceToken: string,
+    options?: Partial<ILogtailOptions>
+  ) {
+    super(sourceToken, options);
+
+    // Sync function
+    const sync = async (logs: ILogtailLog[]): Promise<ILogtailLog[]> => {
+      const res = await fetch(
+        this._options.endpoint,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/msgpack",
+            Authorization: `Bearer ${this._sourceToken}`,
+            "User-Agent": "logtail-js(node)"
+          },
+          body: this.encodeAsMsgpack(logs)
+        }
+      );
+
+      if (res.ok) {
+        return logs;
+      }
+
+      throw new Error(res.statusText);
+    };
+
+    // Set the throttled sync function
+    this.setSync(sync);
+  }
+
+  /**
+   * Override `Base` log to enable Node.js streaming
+   *
+   * @param message: string - Log message
+   * @param level (LogLevel) - Level to log at (debug|info|warn|error)
+   * @param log: (Partial<ILogtailLog>) - Initial log (optional)
+   * @returns Promise<ILogtailLog> after syncing
+   */
+  public async log<TContext extends Context>(
+    message: string,
+    level?: LogLevel,
+    context: TContext = {} as TContext
+  ) {
+    // Process/sync the log, per `Base` logic
+    context = { ...getStackContext(this), ...context };
+    const processedLog = await super.log(message, level, context);
+
+    // Push the processed log to the stream, for piping
+    if (this._writeStream) {
+      this._writeStream.write(JSON.stringify(processedLog) + "\n");
+    }
+
+    // Return the transformed log
+    return processedLog as ILogtailLog & TContext;
+  }
+
+  /**
+   * Pipe JSON stringified `ILogtailLog` to a stream after syncing
+   *
+   * @param stream - Writable|Duplex stream
+   */
+  public pipe(stream: Writable | Duplex) {
+    this._writeStream = stream;
+    return stream;
+  }
+
+  private encodeAsMsgpack(logs: ILogtailLog[]): Buffer {
+    const logsWithISODateFormat = logs.map((log) => ({ ...log, dt: log.dt.toISOString() }));
+    const encoded = encode(logsWithISODateFormat);
+    const buffer = Buffer.from(encoded.buffer, encoded.byteOffset, encoded.byteLength)
+    return buffer;
+  }
+}
---
+++ pages/api/hello.js
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { Node as Logtail } from '@logtail/js';
+
+const logger = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN);
+
+export default function handler(req, res) {
+  const user = {
+    id: 123,
+    name: 'John Doe',
+  };
+
+  logger.info(
+    'logging an API request',
+    { user } // you can log structured data
+  );
+
+  res.status(200).json({ name: user.name })
+}
